74f6b0aeb827dd31513ad9bcaa34e5056e77040d
Morozov Mikhail <memphis.sail@gmail.com>
2025-11-23 20:42:40 +1000

    feat(pagination): add pagination component

---
+++ src/shared/ui/Pagination/Pagination.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { IconChevronRight } from "../icons";
+
+const { pagesCount } = defineProps<{ pagesCount: number }>();
+const currentPage = defineModel<number>({ required: true });
+</script>
+
+<template>
+  <section class="pagination">
+    <button
+      class="pagination__page"
+      @click="currentPage > 1 ? currentPage-- : null"
+    >
+      <IconChevronRight style="rotate: 180deg" />
+      <span class="pagination__page-prev">PREVIOUS</span>
+    </button>
+    <button
+      v-for="page in pagesCount"
+      :key="page"
+      class="pagination__page"
+      :class="{ 'pagination__page-active': page === currentPage }"
+      @click="currentPage !== page ? (currentPage = page) : null"
+    >
+      {{ page }}
+    </button>
+    <button
+      class="pagination__page"
+      @click="currentPage < pagesCount ? currentPage++ : null"
+    >
+      <span class="pagination__page-next">NEXT</span>
+      <IconChevronRight />
+    </button>
+  </section>
+</template>
+
+<style lang="scss">
+@use "styles";
+</style>
